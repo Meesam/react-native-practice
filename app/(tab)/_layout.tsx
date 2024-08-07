14044d2e7a9e30dc267881a0c468ea539e51ab49
Meesam <meesam.engineer@gmail.com>
2024-08-07 19:43:22 +0530

    animation practice

--- app/(tab)/_layout.tsx
+++ app/(tab)/_layout.tsx
@@ -1,48 +1,74 @@
-import { View, Text, TextInput,StyleSheet } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
-import { Feather, FontAwesome } from '@expo/vector-icons'
-
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { Feather, FontAwesome } from "@expo/vector-icons";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const LogoTitle: React.FC<any> = () => {
   return (
-    <View style={styles.logoTitle}>
-      <FontAwesome name="search" color={"gray"} size={20} />
-      <TextInput style={styles.search} placeholder="Search" />
-      <Feather name="more-vertical" size={20} color="gray" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.logoContainer}>
+        <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <TextInput style={styles.search} placeholder="Search" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 const Tablayout = () => {
   return (
     <Tabs>
-       <Tabs.Screen name="index" options={{ title: 'Home', headerShadowVisible: true ,tabBarIcon:({size,color})=>{
-         return <FontAwesome name="home" color={'blue'} size={25}/>
-       } }} />
-       <Tabs.Screen name="search" options={{ title: 'Search', tabBarIcon:({size,color})=>{
-         return <FontAwesome name="search" color={'blue'} size={25}/>
-       } }} />
-       <Tabs.Screen name="profile" options={{ title: 'Profile', tabBarIcon:({size,color})=>{
-         return <FontAwesome name="user" color={'blue'} size={25}/>
-       } }} />
-    </Tabs>
-  )
-}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShadowVisible: true,
+          tabBarIcon: ({ size, color }) => {
+            return <FontAwesome name="home" color={"blue"} size={25} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          header: (props) => {
+            return <LogoTitle {...props} />;
+          },
+          tabBarIcon: ({ size, color }) => {
+            return <FontAwesome name="search" color={"blue"} size={25} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
 
-export default Tablayout
+          tabBarIcon: ({ size, color }) => {
+            return <FontAwesome name="user" color={"blue"} size={25} />;
+          },
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default Tablayout;
 
 const styles = StyleSheet.create({
-  logoTitle: {
-    flexDirection: "row",
+  logoContainer: {
     alignItems: "center",
     flex: 1,
+    flexDirection: "row",
+    marginTop: 30,
   },
   search: {
     borderColor: "gray",
     borderWidth: 1,
     paddingLeft: 10,
-    borderRadius: 15,
+    borderRadius: 10,
     width: 350,
-    height: 40,
+    height: 50,
+    fontSize: 16,
   },
 });
--- app/(tab)/profile.tsx
+++ app/(tab)/profile.tsx
@@ -1,12 +1,56 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native'
 import React from 'react'
+import Animated, { useAnimatedStyle, useSharedValue, withSpring, withSequence, withRepeat,withTiming } from 'react-native-reanimated'
+
 
 const Profile = () => {
+  const sharedWidth = useSharedValue(0)
+  const offset = useSharedValue(0)
+  const OFFSET = 20
+  const TIME = 250
+  const handlePress = ()=>{
+    sharedWidth.value = withSpring(sharedWidth.value + 10)
+  }
+
+  const handlePressShake = ()=>{
+     offset.value = withSequence(
+       withTiming(-OFFSET, { duration: TIME }),
+       withRepeat(withTiming(OFFSET, { duration: TIME/2 }), 5, true),
+       withTiming(0,{duration:TIME})
+     )
+  }
+
+  const animatedStyle = useAnimatedStyle(()=>({
+     transform:[{
+      translateX: withSpring(sharedWidth.value * 2)
+    }]
+  }))
+
+  const shakeStyle = useAnimatedStyle(()=>({
+    transform:[{
+      translateX: offset.value
+    }]
+  }))
+  
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'flex-start' }}>
+       <Animated.View style={[styles.box, animatedStyle]} />
+       <Button title='Click me' onPress={handlePress} />
+       <Animated.View style={[styles.box, shakeStyle]}>
+         <Text>Shake</Text>
+       </Animated.View>
+       <Button title='Shake it' onPress={handlePressShake} />
     </View>
   )
 }
 
 export default Profile
+
+const styles = StyleSheet.create({
+  box:{
+    height: 50, 
+    backgroundColor: 'violet', 
+    marginBottom:20,
+    width:50
+  }
+});
